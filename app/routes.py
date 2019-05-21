179297188dfe23916039ab1bdc311c781422163d
Sudharsan Ramakrishnan <sramakrishnan@configit.com>
2019-05-22 02:16:24 +0530

    using lstm to predict closing stock value

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,13 @@
 from flask import redirect, jsonify, request
 from app import app
-from app.linear_regression import simple_linear_regression, rmse_metric
+from math import sqrt
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, LSTM
 from werkzeug.utils import secure_filename
 import os, json
 import pandas as pd
+import numpy as np
 
 ALLOWED_EXTENSIONS = set(['csv','xlsx'])
 DATA_FRAME = None
@@ -42,7 +46,7 @@ def get_details(name):
     data['actual'] = DATA_FRAME.get_description(name)
     data['predicted'], data['rmse'] = DATA_FRAME.get_predicted(name)
     response = app.response_class(
-        response=json.dumps(data),
+        response=json.dumps(data, cls=NumpyEncoder),
         status=200,
         mimetype='application/json'
     )
@@ -78,21 +82,92 @@ class DataFrame:
         return get_stats(groups)
 
     def get_predicted(self,company):
-        stock = self.df.loc[self.df['symbol'] == company].set_index('timestamp')
+        stock = self.df.loc[self.df['symbol'] == company]
+        stock['timestamp'] = pd.to_datetime(stock.timestamp,format='%Y-%m-%d')
+        stock.index = stock['timestamp']
         actual = stock.last('6M')['close'].tolist()
-        dataset, test_set = [], []
+        data = stock.sort_index(ascending=True, axis=0)
 
-        current_df = stock.first('18M')
-        for index, row in current_df.iterrows():
-            dataset.append([(row['high']+row['low'])/2, row['close']])
-         
-        current_df = stock.last('6M')
-        for index, row in current_df.iterrows():
-            test_set.append([(row['high']+row['low'])/2, 0])
+        new_data = pd.DataFrame(index=range(0,len(self.df)),columns=['Date', 'close'])
+        for i in range(0,len(data)):
+            new_data['Date'][i] = data['timestamp'][i]
+            new_data['close'][i] = data['close'][i]
+
+        #setting index
+        new_data.index = new_data.Date
+        new_data.drop('Date', axis=1, inplace=True)
+
+        #creating train and test sets
+        dataset = new_data.values
+        train_size = len(stock.first('18M'))
+        train = dataset[0:train_size,:]
+        valid = dataset[train_size:,:]
+
+        #converting dataset into x_train and y_train
+        scaler = MinMaxScaler(feature_range=(0, 1))
+        scaled_data = scaler.fit_transform(dataset)
+
+        x_train, y_train = [], []
+        for i in range(60,len(train)):
+            x_train.append(scaled_data[i-60:i,0])
+            y_train.append(scaled_data[i,0])
+        x_train, y_train = np.array(x_train), np.array(y_train)
+
+        x_train = np.reshape(x_train, (x_train.shape[0],x_train.shape[1],1))
+
+        # create and fit the LSTM network
+        model = Sequential()
+        model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1],1)))
+        model.add(LSTM(units=50))
+        model.add(Dense(1))
+
+        model.compile(loss='mean_squared_error', optimizer='adam')
+        model.fit(x_train, y_train, epochs=1, batch_size=1, verbose=2)
+
+        #predicting 246 values, using past 60 from the train data
+        inputs = new_data[len(new_data) - len(valid) - 60:].values
+        inputs = inputs.reshape(-1,1)
+        inputs  = scaler.transform(inputs)
+
+        X_test = []
+        for i in range(60,inputs.shape[0]):
+            X_test.append(inputs[i-60:i,0])
+        X_test = np.array(X_test)
+
+        X_test = np.reshape(X_test, (X_test.shape[0],X_test.shape[1],1))
+        closing_price = model.predict(X_test)
+        closing_price = scaler.inverse_transform(closing_price)
         
-        current_df['close'] = simple_linear_regression(dataset, test_set)
+        valid = new_data[train_size:]
+        valid['Predictions'] = closing_price
+        valid = valid.dropna()
+
+        current_df = stock.last('6M')
+        current_df['close'] = valid.last('6M')['Predictions']
+
         rmse = round(rmse_metric(actual, current_df['close'].tolist()), 3)
+
         return get_stats(current_df.groupby(pd.Grouper(freq='2W'))), rmse
 
+def rmse_metric(actual, predicted):
+	sum_error = 0.0
+	for i in range(len(actual)):
+		prediction_error = predicted[i] - actual[i]
+		sum_error += (prediction_error ** 2)
+	mean_error = sum_error / float(len(actual))
+	return sqrt(mean_error)
 
+class NumpyEncoder(json.JSONEncoder):
+    """ Special json encoder for numpy types """
+    def default(self, obj):
+        if isinstance(obj, (np.int_, np.intc, np.intp, np.int8,
+            np.int16, np.int32, np.int64, np.uint8,
+            np.uint16, np.uint32, np.uint64)):
+            return int(obj)
+        elif isinstance(obj, (np.float_, np.float16, np.float32, 
+            np.float64)):
+            return float(obj)
+        elif isinstance(obj,(np.ndarray,)): #### This is the fix
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
    
